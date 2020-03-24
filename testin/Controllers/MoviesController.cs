4a0aaaed3f79293be261c335200dda5a1dc40c27
sagithiyan <sagithiyannagendran@gmail.com>
2020-03-24 10:53:44 +0530

    Add project files.

---
+++ testin/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using testin.Models;
+
+namespace testin.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "sachin" };
+                return View(movie);
+            
+
+        }
+    }
+}
